93360aa82d86a4a8ecc2ad6f38d9424e242ce686
noahrel0293 <gxnd0293@leeds.ac.uk>
2026-02-28 14:21:19 +0000

    first commit

--- worksheet/task_2/cinema.py
+++ worksheet/task_2/cinema.py
@@ -18,6 +18,13 @@ def customer_tickets(conn, customer_id):
     Include only tickets purchased by the given customer_id.
     Order results by film title alphabetically.
     """
+    customer_tickets_query = "SELECT films.title, screenings.screen, tickets.price FROM (tickets INNER JOIN screenings ON tickets.screening_id = screenings.screening_id) INNER JOIN films ON screenings.film_id = films.film_id WHERE customer_id = ?;"
+    customer_tickets_cursor = conn.execute(customer_tickets_query, (customer_id,))
+    customer_tickets_list = []
+    for row in customer_tickets_cursor:
+        customer_tickets_list.append(row)
+    customer_tickets_list.sort(key=lambda customer_tickets_list:customer_tickets_list[0])
+    return customer_tickets_list
     pass
 
 
@@ -29,6 +36,13 @@ def screening_sales(conn):
     Include all screenings, even if tickets_sold is 0.
     Order results by tickets_sold descending.
     """
+    screening_sales_query = "SELECT screenings.screening_id, films.title, COUNT(tickets.ticket_id) FROM (screenings LEFT JOIN tickets ON screenings.screening_id = tickets.screening_id) INNER JOIN films ON screenings.film_id = films.film_id GROUP BY screenings.screening_id;"
+    screening_sales_cursor = conn.execute(screening_sales_query)
+    screening_sales_list = []
+    for row in screening_sales_cursor:
+        screening_sales_list.append(row)
+    screening_sales_list.sort(key=lambda screening_sales_list:screening_sales_list[2], reverse=True)
+    return screening_sales_list
     pass
 
 
@@ -42,4 +56,10 @@ def top_customers_by_spend(conn, limit):
     Order by total_spent descending.
     Limit the number of rows returned to `limit`.
     """
+    top_customers_by_spend_query = "SELECT customers.customer_name, SUM(tickets.price) FROM tickets INNER JOIN customers ON tickets.customer_id = customers.customer_id GROUP BY tickets.customer_id ORDER BY SUM(tickets.price) DESC, customers.customer_name ASC LIMIT ?;"
+    top_customers_by_spend_cursor = conn.execute(top_customers_by_spend_query, (limit,))
+    top_customers_by_spend_list = []
+    for row in top_customers_by_spend_cursor:
+        top_customers_by_spend_list.append(row)
+    return top_customers_by_spend_list
     pass
